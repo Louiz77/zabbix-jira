39bce6b2f0cd84c31ab35a76c8f6fa44ea497e59
Louiz77 <luizgu009@gmail.com>
2024-12-12 15:51:43 -0300

    report.log fix

--- app/register_log.py
+++ app/register_log.py
@@ -1,8 +1,9 @@
 from datetime import datetime
+from config import Config
 
 def logger(message):
     try:
-        with open(f"./log/report.log", "a") as my_file:
+        with open(f"{Config.LOG_FOLDER}report.log", "a") as my_file:
             my_file.write(f"-{datetime.now()} | {message}\n")
     except Exception as e:
         print(e)
