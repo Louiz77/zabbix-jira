633c75c70a7fc4c3a84d3d1cff4fdf74215031c0
Louiz77 <luizgu009@gmail.com>
2024-12-12 15:38:48 -0300

    LOG_FOLDER for CONFIG.PY

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ class Config:
     JIRA_API_TOKEN = os.getenv('JIRA_API_TOKEN')
     JIRA_API_EMAIL = os.getenv('JIRA_API_EMAIL')
     WHATSAPP_API_URL = 'http://127.0.0.1:7000/send-message'
+    LOG_FOLDER = os.getenv('LOG_FOLDER')
